9142ff87cf9b7693e59a263412ffb0815b654452
DevExpressExampleBot <fakeEmail>
2022-12-17 14:45:43 +0400

    Source auto update [skip ci]

--- VB/AppBarExample/MainWindow.xaml.vb
+++ VB/AppBarExample/MainWindow.xaml.vb
@@ -1,35 +1,24 @@
-﻿Imports System
-Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
-Imports System.Threading.Tasks
 Imports System.Windows
 Imports System.Windows.Controls
-Imports System.Windows.Data
-Imports System.Windows.Documents
-Imports System.Windows.Input
-Imports System.Windows.Media
-Imports System.Windows.Media.Imaging
-Imports System.Windows.Navigation
-Imports System.Windows.Shapes
 
 Namespace AppBarExample
+
     ''' <summary>
     ''' Interaction logic for MainWindow.xaml
     ''' </summary>
-    Partial Public Class MainWindow
+    Public Partial Class MainWindow
         Inherits Window
 
         Public Sub New()
-            InitializeComponent()
+            Me.InitializeComponent()
         End Sub
 
         Private Sub OnMoveAppBarTop(ByVal sender As Object, ByVal e As RoutedEventArgs)
-            appBar.Alignment = DevExpress.Xpf.WindowsUI.AppBarAlignment.Top
+            Me.appBar.Alignment = DevExpress.Xpf.WindowsUI.AppBarAlignment.Top
         End Sub
 
         Private Sub OnMoveAppBarBottom(ByVal sender As Object, ByVal e As RoutedEventArgs)
-            appBar.Alignment = DevExpress.Xpf.WindowsUI.AppBarAlignment.Bottom
+            Me.appBar.Alignment = DevExpress.Xpf.WindowsUI.AppBarAlignment.Bottom
         End Sub
     End Class
 End Namespace
